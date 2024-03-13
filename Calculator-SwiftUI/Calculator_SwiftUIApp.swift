bf5111b9fd8a2490ed911faf061f2b0a9fdd26b8
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-03-13 14:47:27 +0530

    Initial Commit

---
+++ Calculator-SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator-SwiftUI
+//
+//  Created by Anuradha Desilva on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
